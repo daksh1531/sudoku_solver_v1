7384e28595763338850eef88c99d5fe83f64047d
daksh1531 <39877584+daksh1531@users.noreply.github.com>
2020-12-24 10:23:40 +0530

    Add files via upload

---
+++ sudoku_solver.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class sudoku here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class sudoku_solver
+{
+        public int sudoku[][] = {
+            {5,3,0,0,7,0,0,0,0},
+            {6,0,0,1,9,5,0,0,0},
+            {0,9,8,0,0,0,0,6,0},
+            {8,0,0,0,6,0,0,0,3},
+            {4,0,0,8,0,3,0,0,1},
+            {7,0,0,0,2,0,0,0,6},
+            {0,6,0,0,0,0,2,8,0},
+            {0,0,0,4,1,9,0,0,5},
+            {0,0,0,0,8,0,0,7,9}
+        };
+        
+        public int solved_sudoku [][] = 
+}
